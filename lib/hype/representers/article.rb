15189cb342dd9a4a5f95bbef7c81c4c74188e7b4
Jay Hayes <ur@iamvery.com>
2014-10-08 06:26:06 -0500

    wip

--- lib/hype/representers/article.rb
+++ lib/hype/representers/article.rb
@@ -1,13 +1,19 @@
 require 'roar/representer/json'
+require 'roar/representer/feature/hypermedia'
 
 module Hype
   module Representers
     module Article
       include Roar::Representer::JSON
+      include Roar::Representer::Feature::Hypermedia
 
       property :title
       property :body
       property :word_count
+
+      link :self do
+        "//articles/#{title}"
+      end
     end
   end
 end
--- lib/hype/representers/articles.rb
+++ lib/hype/representers/articles.rb
@@ -1,16 +1,22 @@
 require 'roar/representer/json'
+require 'roar/representer/feature/hypermedia'
 require 'hype/representers/article'
 
 module Hype
   module Representers
     module Articles
       include Roar::Representer::JSON
+      include Roar::Representer::Feature::Hypermedia
 
       def articles
         self
       end
 
       collection :articles, extend: Article
+
+      link :self do
+        "//articles"
+      end
     end
   end
 end
